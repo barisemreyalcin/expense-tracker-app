79522fcf448f24be78e74d4da93eca9d7a88b674
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-09-14 19:53:01 +0300

    filter and display expenses by year

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -13,17 +13,26 @@ function ExpenseList(props) {
         console.log(filteredYear);
     }
 
+    const filteredExpenses = props.expenseList.filter(expenseItem => {
+        return expenseItem.date.getFullYear().toString() === selectedYear;
+    })
+
+    let filteredExpenseContent = <p>No expense found.</p>;
+    if(filteredExpenses.length > 0) {
+        filteredExpenseContent = filteredExpenses.map(expenseItem => 
+            <ExpenseItem 
+                key={expenseItem.id}
+                date={expenseItem.date}
+                title={expenseItem.title}
+                price={expenseItem.price}
+            />
+        )
+    }
+
     return (
         <Card className="expense-list">
             <ExpenseFilter filtered={selectedYear} onFilterChangeHandler={filterChangeHandler}/>
-            {props.expenseList.map(expenseItem => 
-                <ExpenseItem 
-                    key={expenseItem.id}
-                    date={expenseItem.date}
-                    title={expenseItem.title}
-                    price={expenseItem.price}
-                />
-            )}
+            {filteredExpenseContent}
         </Card>
     )
 }
